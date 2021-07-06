1535e4043d5344b03a96e588e73624cce243b15f
med2dev <daniel.bryce.hall@gmail.com>
2021-07-05 20:35:24 -0700

    July 5 challenge

---
+++ cha2021-07-05/app.js
@@ -0,0 +1,15 @@
+// P: x1, x2, floats, representing the two roots of a quadratic polynomial
+// R: an array, length 3, the coefficients of the corresponding quadratic 
+//  polynomial in the form: ax^2 + bx + c = 0, where a = 1
+
+// E:
+quadratic(0,1)    //  [1, -1, 0]
+quadratic(1,1)    //  [1, -2, 1]
+quadratic(-4,-9)  // [1, 13, 36]
+quadratic(-5,-4)  //  [1, 9, 20]
+quadratic(4,-9)   // [1, 5, -36]
+quadratic(5,-4)   // 1, -1, -20]
+
+function quadratic(x1, x2){
+    
+}
