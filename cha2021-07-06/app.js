cb4bbfb9a9fcf12b6c0455bdd5a43e2a041e4eb7
med2dev <daniel.bryce.hall@gmail.com>
2021-07-06 20:41:10 -0700

    July 6 challenge

---
+++ cha2021-07-06/app.js
@@ -0,0 +1,6 @@
+// this is a basic challenge. Create a hash table called rooms. Each room in
+// rooms should contain at least 3 properties, name, description, and completed
+
+let rooms = {
+    
+}
