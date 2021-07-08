1989da22552e1486e53ef7db9582fa1881c21c11
med2dev <daniel.bryce.hall@gmail.com>
2021-07-07 20:46:09 -0700

    July 7 challenge

---
+++ cha2021-07-07/app.js
@@ -0,0 +1,12 @@
+// P: language, a string, representing a language to greet a site visitor
+// R: string, the word Welcome, in the requested language
+//      if an invalid is passed, use english
+
+// E:
+greet('english') // 'Welcome'
+greet('dutch') // 'Welkom'
+greet('IP_ADDRESS_INVALID') // 'Welcome'
+
+function greet(language){
+    
+}
