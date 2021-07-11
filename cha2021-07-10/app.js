bfb017ee39144911be4d44706efef5a5139997cf
med2dev <daniel.bryce.hall@gmail.com>
2021-07-10 21:20:22 -0700

    July 10 challenge

---
+++ cha2021-07-10/app.js
@@ -0,0 +1,20 @@
+// P: a string using Polish spelling that may contain letters with diacritics
+
+// R: the string, with the diacritic removed
+//      that is, make the following changes
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+correctPolishLetters("Jędrzej Błądziński")          // "Jedrzej Bladzinski"
+correctPolishLetters("Lech Wałęsa")                 // "Lech Walesa"
+correctPolishLetters("Maria Skłodowska-Curie")      // "Maria Sklodowska-Curie"
+
+function correctPolishLetters(s){
+
+}
