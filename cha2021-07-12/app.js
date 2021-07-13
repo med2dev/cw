33a848fc1545d566a4c96bae914e81f0d3e733fa
med2dev <daniel.bryce.hall@gmail.com>
2021-07-12 23:19:19 -0700

    July 12 challenge

---
+++ cha2021-07-12/app.js
@@ -0,0 +1,10 @@
+// P: a float
+// R: a string with the float formated as USD currency
+//      that is, prepend a '$', and enforce 2 positions after the decimal
+
+// E: 
+formatMoney(5.6) // "$5.60"
+
+function formatMoney(amount){
+
+}
