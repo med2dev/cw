0790f5c13a7479f5ab81094c4bfd91b353118a5e
med2dev <daniel.bryce.hall@gmail.com>
2021-07-12 23:25:06 -0700

    refactor with NumberFormat object

--- cha2021-07-12/app.js
+++ cha2021-07-12/app.js
@@ -11,3 +11,14 @@ function formatMoney(amount){
     // prepend a '$' with concatenation
     return "$" +  amount.toFixed(2)
 }
+
+// Using a NumberFormat object
+function formatMoney(amount){
+    const usd = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      useGrouping: false
+    });
+    return usd.format(amount);
+  }
