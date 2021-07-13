a228236b2e7eeea45819866010e3dab2eebed87a
med2dev <daniel.bryce.hall@gmail.com>
2021-07-12 23:23:29 -0700

    IMplement formatMoney

--- cha2021-07-12/app.js
+++ cha2021-07-12/app.js
@@ -6,5 +6,8 @@
 formatMoney(5.6) // "$5.60"
 
 function formatMoney(amount){
-
+    // Number.prototype.toFixed(2) will enforce 2 decimal positions 
+    // and coerce to string
+    // prepend a '$' with concatenation
+    return "$" +  amount.toFixed(2)
 }
