b8a799118056a27cf58ebfc55dbcf6fc3752b52f
med2dev <daniel.bryce.hall@gmail.com>
2021-07-15 23:38:05 -0700

    July 15 challenge

---
+++ cha2021-07-15/app.js
@@ -0,0 +1,12 @@
+// P: the average velocity of an object
+// R: the time that object will take to travel 400 units of length
+//      if the velocity is negative, return 0
+// E:
+
+sakuraFall(5) // 80
+sakuraFall(10) // 40
+sakuraFall(-1) // 0
+
+function sakuraFall(v){
+
+}
