cd06348eb7575c359ecbf019a407d885151907be
med2dev <daniel.bryce.hall@gmail.com>
2021-07-17 23:56:36 -0700

    Implement parseF

--- cha2021-07-17/app.js
+++ cha2021-07-17/app.js
@@ -4,6 +4,11 @@
 // E:
 parseF('1') // '1.0'
 
+// really, this challenge is asking you to convert a builtin to a custom
+// function by slightly altering the behavior
+// turn NaN into null
+// This requires knowing how to test for NaN
 function parseF(s){
-
+    const floated = parseFloat(s)
+    return isNaN(floated) ? null : floated
 }
