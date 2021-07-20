72e725ad6efe2e641c3213f56f9928aa6c0c7bd9
med2dev <daniel.bryce.hall@gmail.com>
2021-07-19 23:57:01 -0700

    July 19 challenge

---
+++ cha2021-07-19/app.js
@@ -0,0 +1,12 @@
+// P: an array of positive integer, arr, and a nonneative integer, n
+
+// R: the nth element of arr raised to the nth power
+//      if there is no nth element, return -1
+
+// E:
+index([1, 2, 3, 4], 2) // 9
+index([1, 2, 3, 4], 4) // -1
+
+function index(arr, n ){
+
+}
