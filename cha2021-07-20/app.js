88888de3e55471c529d06d95bfe20be298e615ec
med2dev <daniel.bryce.hall@gmail.com>
2021-07-20 23:50:47 -0700

    July 20 challenge

---
+++ cha2021-07-20/app.js
@@ -0,0 +1,11 @@
+// P: two numbers, mpg and ppg (minutes per game and points per game)
+// R: the extrapolation of ppg to a full 48 minute basketball game
+
+// E:
+pointsPer48(12, 20) // 28.8
+pointsPer48(10, 10) // 48.0
+pointsPer48(5, 17)  // 14.1
+
+function pointsPer48(ppg, mpg){
+
+}
