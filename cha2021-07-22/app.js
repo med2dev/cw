f181609f6401bd6f970139e29a80ac3b8b279f9c
med2dev <daniel.bryce.hall@gmail.com>
2021-07-22 01:02:58 -0700

    documentation

--- cha2021-07-22/app.js
+++ cha2021-07-22/app.js
@@ -10,3 +10,14 @@ arr(2) // [0, 1]
 
 
 const arr = n => Array.from({length: n || 0}, (v, k) => k)
+
+// you don't, in fact, need to use n || 0
+// when length: undefined is used, you get an empty array
+// compare: Array.from({length: x}), where x isn't defined
+//          here you get an error
+// vs:      Array.from({length: undefined})
+//          produces an empty array
+
+// this is a good exercise in how to set a default value
+// for a parameter in an arrow function. 
+// i.e., you can't. so instead, when you use n, use || value
