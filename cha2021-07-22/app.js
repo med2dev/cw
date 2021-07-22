026a4b5356a1dc016165984da9bb5d401f5479f6
med2dev <daniel.bryce.hall@gmail.com>
2021-07-22 00:40:11 -0700

    July 22 challenge

---
+++ cha2021-07-22/app.js
@@ -0,0 +1,11 @@
+// P: an integer, n
+// R: an array of length n, with consecutive nonnegative integers 
+//      beginning with 0
+// Edge case: when called with no value, return an empty array
+
+// E:
+arr() // []
+arr(2) // [0, 1]
+
+
+
