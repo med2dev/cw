111a3552692ec737a8170a1cb502f73a220ccd57
med2dev <daniel.bryce.hall@gmail.com>
2021-07-23 01:19:29 -0700

    July 23 challenge

---
+++ cha2021-07-23/app.js
@@ -0,0 +1,12 @@
+// P: n, a nonnegative integer
+// R: a decimal number that corresponds to the binary representation
+
+// E:
+toBinary(1) // 1
+toBinary(2) // 10
+toBinary(3) // 11
+toBinary(4) // 100
+
+function toBinary(n){
+
+}
