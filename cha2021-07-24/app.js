aaf6baae9dbb55a033da52e0e7c3e0edb317e6d4
med2dev <daniel.bryce.hall@gmail.com>
2021-07-24 01:32:34 -0700

    July 24 challenge

---
+++ cha2021-07-24/app.js
@@ -0,0 +1,12 @@
+// P: two numbers, coefficient, and exponent
+// R: a string representing the derivative of a polynomial term with that 
+//      coefficient and exponent
+//      
+
+// E:
+derive(7, 8) // '56x^7'
+derive(5, 9) // '45x^8
+
+function derive(coefficient, exponent){
+
+}
