f3f4d193acde831c0aca94333771870d654f3ba2
med2dev <daniel.bryce.hall@gmail.com>
2021-07-27 14:45:05 -0700

    examples and pseudocode

--- cha2021-07-27/app.js
+++ cha2021-07-27/app.js
@@ -1,2 +1,12 @@
 // debug challenge
+
+// P: two numbers
+// R: the first divided by the second
+
+
+// E: 
+solve(4, 1) // 4
+solve(5, 0) // Infinity
+
+// P: typo here is double /, which is the syntax for a comment
 const solve = (x, y) => x // y
