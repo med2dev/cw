03d5c45dc5c0066554ae672f314c3cb7d1a4531a
med2dev <daniel.bryce.hall@gmail.com>
2021-07-26 23:31:00 -0700

    July 26 challenge

---
+++ cha2021-07-26/app.js
@@ -0,0 +1,9 @@
+// P: integer and boolean, representing a salary and bonus
+// R: total compensation as a string prefixed with £, where if bonus is true,
+//      total comp is salary * 10
+bonusTime(10000, true) // '£100000'
+
+
+function bonusTime(salary, bonus){
+
+}
