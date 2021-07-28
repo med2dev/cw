040962d51919ffd1de12ae55e2c1c949711c957b
med2dev <daniel.bryce.hall@gmail.com>
2021-07-28 14:50:34 -0700

    July 28 challenge

---
+++ cha2021-07-28/app.js
@@ -0,0 +1,21 @@
+// P: a string, representing a bar patron
+// R: a string, representing their drink order
+// drink orders are determined as follows
+
+//  Patron               Drink
+//  default             'Beer'
+// 'Jabroni'            'Patron Tequila'
+// 'School Counselor'   'Anything with Alcohol'
+// 'Programmer'         'Hipster Craft Beer'
+// 'Bike Gang Member'   'Moonshine'
+// 'Politician'         'Your tax dollars'
+// 'Rapper'             'Cristal'
+
+// input should be case insensitive
+
+// E:
+getDrinkByProfession('poLiTician') // 'Your tax dollars'
+
+function getDrinkByProfession(patron){
+
+}
