fdf15fcb422f097cb4e0aaaaa466b9bb5a940048
med2dev <daniel.bryce.hall@gmail.com>
2021-07-29 15:25:02 -0700

    implement nextId

--- cha2021-07-29/app.js
+++ cha2021-07-29/app.js
@@ -7,7 +7,9 @@ nextId([0, 1, 2, 3, 4, 5, 6]) // 7
 
 function nextId(ids){
     // sort the array
+    const sortedIds = ids.sort((a, b) => a - b)
     // filter array for values that are equal to their index
+    return sortedIds.filter((e, i) => e == i).pop() + 1
     // add one to the last
 
 }
