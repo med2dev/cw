a16a808ad7b5410f0b2e2f2b74332a5d7702086b
med2dev <daniel.bryce.hall@gmail.com>
2021-07-29 15:29:49 -0700

    handled duplicates

--- cha2021-07-29/app.js
+++ cha2021-07-29/app.js
@@ -6,12 +6,13 @@ nextId([0, 1, 2, 3, 5]) // 4
 nextId([0, 1, 2, 3, 4, 5, 6]) // 7
 
 function nextId(ids){
-    // sort the array
-    const sortedIds = ids.sort((a, b) => a - b)
+    // remove duplicates
+    const setIds = new Set(ids)
+    // sort 
+    const sortedIds = [...setIds].sort((a, b) => a - b)
     // filter array for values that are equal to their index
     return sortedIds.filter((e, i) => e == i).pop() + 1
     // add one to the last
 
 }
 
-// TODO: handle array without duplicates
