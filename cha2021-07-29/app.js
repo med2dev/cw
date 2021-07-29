b8c0da50fb450a1ed9bad4bc89e21214f6f43f8e
med2dev <daniel.bryce.hall@gmail.com>
2021-07-29 15:19:44 -0700

    pseudocode

--- cha2021-07-29/app.js
+++ cha2021-07-29/app.js
@@ -6,5 +6,8 @@ nextId([0, 1, 2, 3, 5]) // 4
 nextId([0, 1, 2, 3, 4, 5, 6]) // 7
 
 function nextId(ids){
+    // sort the array
+    // filter array for values that are equal to their index
+    // add one to the last
 
 }
