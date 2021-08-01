6f5aeccd955fb1f67f7caab839792f11abfec715
med2dev <daniel.bryce.hall@gmail.com>
2021-07-31 23:41:38 -0700

    July 31 challenge

---
+++ cha2021-07-31/app.js
@@ -0,0 +1,9 @@
+// P: an array of integers
+// R: an array with the odd elements of the input
+//      use filter with an arrow function
+// E:
+odds([2, 4, 6]) // []
+odds([1, 2, 3, 5]) // [1, 3, 5]
+function odds(values){
+    return values.filter( => )
+}
