185ebe3b6338f8b72616a79f3dfec538f6dfaf6f
med2dev <daniel.bryce.hall@gmail.com>
2021-08-02 14:22:57 -0700

    August 2 challenge

---
+++ cha2021-08-02/app.js
@@ -0,0 +1,14 @@
+// object challenge: Create a class 'Ball'. Ball objecyts should accept one argument
+// P: string, ball type
+//      if no arguments, ball objects should instantiate with a 'ball type' of 'regular'
+
+// E:
+let ball1 = new Ball()
+let ball2 = new Ball('super')
+
+ball1.ballType // 'regular'
+ball2.ballType // 'super'
+
+const Ball = function(ballType){
+    
+}
