b37582a686851a662e5cd4ebadb036e749476b9d
med2dev <daniel.bryce.hall@gmail.com>
2021-08-03 15:29:36 -0700

    August 3 challenge

---
+++ cha2021-08-03/app.js
@@ -0,0 +1,20 @@
+// P: an array of strings, containing one 'wolf' with the remaining elements 
+//  'sheep'
+// R: a string, based on the following decision rule
+//  sheep are numbered from the end to the beginning, with the last sheep in 
+//  the array being 1, second to last being 2
+//  
+//      if the wolf is the last element in the array, return:
+//      'Pls go away and stop eating my sheep'
+//      Otherwise, return:
+//      'Oi! Sheep number N! You are about to be eaten by a wolf!'
+//      where N is the sheep number
+
+// R:
+warnTheSheep(['sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'sheep']) // 'Oi! Sheep number 2! You are about to be eaten by a wolf!'
+
+
+function warnTheSheep(queue){
+    
+
+}
