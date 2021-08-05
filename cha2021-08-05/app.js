ad86fca3a3ce81ec0230b8e3caf25eb49b803095
med2dev <daniel.bryce.hall@gmail.com>
2021-08-05 15:55:38 -0700

    August 5 challenge

---
+++ cha2021-08-05/app.js
@@ -0,0 +1,15 @@
+// P: two arrays: teams, two teams who played a match, 
+//      and scores, their respective scores
+
+// R: a string describing the match, in the form: 
+//      At match team1 - team2, winning team won!
+//      At match team 1 - team 2, teams played draw.
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+
+function uefaEuro2016(teams, scores){
+
+}
