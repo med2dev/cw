d25313a7074a629ae072c802f9c0ab0bdf096343
med2dev <daniel.bryce.hall@gmail.com>
2021-08-07 17:17:55 -0700

    Implement isWorthIt

--- cha2021-08-07/app.js
+++ cha2021-08-07/app.js
@@ -19,4 +19,7 @@ emptyShip.isWorthIt() // false
 function Ship(draft, crew){
     this.draft = draft
     this.crew = crew 
+    this.isWorthIt = function(){
+        return this.draft - (this.crew * 1.5) > 20
+    }
 }
