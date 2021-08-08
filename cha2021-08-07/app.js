415c4dad0049bd7ca8633b086e3b8fefd872eec4
med2dev <daniel.bryce.hall@gmail.com>
2021-08-07 17:16:12 -0700

    August 7 challenge

---
+++ cha2021-08-07/app.js
@@ -0,0 +1,21 @@
+// Object oriented challenge: create a custom class, Ship
+//      include in the constructor, the properties draft and crew
+//      define a method, isWorthIt, that determines whether the 
+//      draft attributable to cargo is more than 20
+//          the average crew member adds 1.5 draft units
+//          so, to find the draft attributable to cargo, 
+//          subtract crew*1.5 from the draft
+
+
+// P: (for isWorthIt), none. Uses existing properties of the object
+// R: (isWorthIt) Boolean, is the draft attributable to cargo > 20
+
+// E:
+
+
+let emptyShip = new Ship(0, 0)
+emptyShip.isWorthIt() // false
+
+function Ship(draft, crew){
+
+}
