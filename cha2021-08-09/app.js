db1e472125831d1471489a7cf1b36834ae9bb8f2
med2dev <daniel.bryce.hall@gmail.com>
2021-08-09 21:15:08 -0700

    refactor so both validation steps are done using regex

--- cha2021-08-09/app.js
+++ cha2021-08-09/app.js
@@ -8,9 +8,14 @@ validateUsr('jk42_c') // true
 validateUsr('Jk42_c') // false
 
 
-function validateUsr(username) {
+function validateUsrInitial(username) {
     const validLength = 3 < username.length && 17 > username.length
     const validCharacters =  /^[a-z0-9_]*$/.test(username)
     return validLength && validCharacters
 }
 // recall, character classes only match a single character. You need a *
+
+// doh, you can do the length validation and the character class at the same
+function validateUsr(username){
+    return /^[a-z0-9_]{4,16}$/.test(username)
+}
