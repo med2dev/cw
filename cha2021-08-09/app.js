59ed0884b3d32f41645972c79634ed0bbf0fbc4b
med2dev <daniel.bryce.hall@gmail.com>
2021-08-09 20:57:48 -0700

    August 9 challenge

---
+++ cha2021-08-09/app.js
@@ -0,0 +1,13 @@
+// P: a string
+// R: boolean, is the string a valid username
+//      valid usernames are between 4 and 16 characters
+//      and contain only lowercase letters, numbers, or underscores
+
+// E:
+validateUsr('jk42_c') // true
+validateUsr('Jk42_c') // false
+
+
+function validateUsr(username) {
+
+}
