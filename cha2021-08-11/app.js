808fd65fea102c1f619d404bbe0caf86811abae7
med2dev <daniel.bryce.hall@gmail.com>
2021-08-11 22:46:30 -0700

    August 11 challenge

---
+++ cha2021-08-11/app.js
@@ -0,0 +1,9 @@
+// P: two strings, obj and feature
+// R: a string, obj and feature concatenated with 'are, as in 
+
+// E:
+templeStrings('Animals', 'Good') // 'Animals are Good'
+
+function templeStrings(obj, feature){
+
+}
