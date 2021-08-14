7f422062463d0af4697a9e8210d5905b00b902ee
med2dev <daniel.bryce.hall@gmail.com>
2021-08-13 23:13:17 -0700

    August 13 challenge

---
+++ cha2021-08-13/app.js
@@ -0,0 +1,14 @@
+// debug challenge
+
+function correctTail(bod, tail ;
+  
+    sub = body.substr(bodylength-(tail.length)
+    
+    if sub = tail) ;
+      return true
+    }
+    else 
+      return false
+    
+  
+  
