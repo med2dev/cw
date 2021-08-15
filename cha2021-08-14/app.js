ceba317fd1e8482b22146c9830a2ef4ee9d4d484
med2dev <daniel.bryce.hall@gmail.com>
2021-08-14 23:20:28 -0700

    implement divisibleBy

--- cha2021-08-14/app.js
+++ cha2021-08-14/app.js
@@ -2,8 +2,9 @@
 // R: an array containing those elements in numbers that are divislbe by divisor
 
 // E:
+divisibleBy([1,2 , 3, 4, 5, 6], 2) // [2, 4, 6]
 
 
 function divisibleBy(numbers, divisor){
-    
+    return numbers.filter(v => v % divisor == 0)
 }
