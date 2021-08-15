691eebabf3ab3d7721e7695394e855be1b538db6
med2dev <daniel.bryce.hall@gmail.com>
2021-08-14 23:19:02 -0700

    August 14 challenge

---
+++ cha2021-08-14/app.js
@@ -0,0 +1,9 @@
+// P: an array, numbers, and an integer, divisor
+// R: an array containing those elements in numbers that are divislbe by divisor
+
+// E:
+
+
+function divisibleBy(numbers, divisor){
+    
+}
