387c51855e4881be5f7cb7863728aed450ea10d8
med2dev <daniel.bryce.hall@gmail.com>
2021-08-16 10:44:14 -0700

    August 16 challenge

---
+++ cha2021-08-16/app.js
@@ -0,0 +1,10 @@
+// P: an array containing numbers, with mixed type. Some are string 
+//      representations, some are not
+// R: the sum of the values of the array
+
+// E:
+sumMix(['1', '4', 2, '7']) // 14
+
+function sumMix(arr){
+
+}
