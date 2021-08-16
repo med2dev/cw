1da084961558d582443edc3a5fd21c72fc151a7a
med2dev <daniel.bryce.hall@gmail.com>
2021-08-16 10:48:14 -0700

    Implement sumMix

--- cha2021-08-16/app.js
+++ cha2021-08-16/app.js
@@ -1,4 +1,4 @@
-// P: an array containing numbers, with mixed type. Some are string 
+// P: an array containing integers, with mixed type. Some are string 
 //      representations, some are not
 // R: the sum of the values of the array
 
@@ -6,5 +6,9 @@
 sumMix(['1', '4', 2, '7']) // 14
 
 function sumMix(arr){
-
+    // Note: yes, you must coerce each element
+    // otherwise, if you get an example like the above, you'll
+    // end up concatenating 
+    // reduce, coercing and summing elements
+    return arr.reduce((p, c) => parseInt(`${p} + ${c}`))
 }
