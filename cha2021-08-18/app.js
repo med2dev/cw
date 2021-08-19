5fcf278876618f6885203af2d334805ebf75835b
med2dev <daniel.bryce.hall@gmail.com>
2021-08-18 22:56:28 -0700

    August 18 challenge

---
+++ cha2021-08-18/app.js
@@ -0,0 +1,10 @@
+// P: x, n, positive integers
+// R: an array containing the first n multiples of x
+
+// E:
+countBy(1, 10) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+countBy(2, 4) // [2, 4, 6, 8]
+
+function countBy(x, n){
+
+}
