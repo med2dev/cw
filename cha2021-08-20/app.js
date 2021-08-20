f4b2758a39eef72eda9fb7c29778e045dc66b87a
med2dev <daniel.bryce.hall@gmail.com>
2021-08-20 12:58:01 -0700

    August 20 challenge

--- cha2021-08-20/app.js
+++ cha2021-08-20/app.js
@@ -1,9 +1,10 @@
 // debug challenge
 // P: an array
-// R: that array, with the first and last element swapped
+// R: no return. swap the first and last element of the array as a side effect
+
 
 function swapValues() {
-    var args = Array.prototype.slice.call(arguments);
+    var args = arguments[0]
     var temp = args[0];
     args[0] = args[1];
     args[1] = temp;
