a1af858bbbc54fbfbba9050579841e1a572682ac
med2dev <daniel.bryce.hall@gmail.com>
2021-08-20 15:29:57 -0700

    document swapValues

--- cha2021-08-20/app.js
+++ cha2021-08-20/app.js
@@ -2,10 +2,25 @@
 // P: an array
 // R: no return. swap the first and last element of the array as a side effect
 
+// as this is used, a global variable, arr, is assigned, then passed as an argument 
+//  to swapValues
+//  note: this is not really best practice
+
+// E:
+var arr = [1, 2]
+swapValues(arr)
+arr // [2, 1]
 
 function swapValues() {
-    var args = arguments[0]
-    var temp = args[0];
+    // the arguments object is indexible and has a length property
+    let args = arguments[0]
+    // assign the first element of the argument to a temp variable
+    let temp = args[0];
+    // give the second value of the array to the first position
     args[0] = args[1];
+    // give the second position the value of your temp variable
     args[1] = temp;
+    // nothing is returned
 }
+
+// as a side effect, the global variable arr now has swapped values
