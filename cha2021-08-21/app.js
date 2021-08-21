2e589c61873dac0302e69f1b1d62c7a741466247
med2dev <daniel.bryce.hall@gmail.com>
2021-08-21 15:59:15 -0700

    August 21 challenge

---
+++ cha2021-08-21/app.js
@@ -0,0 +1,12 @@
+// P: a, b, logical expressions
+// R: boolean, a xor b, that is, true when exactly one of the two expressions is true
+
+// E:
+xor(true, true) // false
+xor(true, false) // true
+xor(false, true) // true
+xor(false, false) // false
+
+function xor(a, b){
+
+}
