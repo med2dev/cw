b9309ecab8fcc6fa9844d1b0028740230f8dfd0a
med2dev <daniel.bryce.hall@gmail.com>
2021-08-21 16:09:35 -0700

    refactor and type checking

--- cha2021-08-21/app.js
+++ cha2021-08-21/app.js
@@ -10,3 +10,12 @@ xor(false, false) // false
 function xor(a, b){
     return (a && !b) || (!a && b)
 }
+
+function xor2(a, b){
+    // xor is true exactly when a and b are logical and different
+    if (typeof(a) == typeof(b) && typeof(a) == 'boolean'){
+        return a != b
+    } else {
+        throw 'Parameter is not boolean'
+    }
+}
