a4454314f0e65a3dacdc1a8eb939073ac43c08a0
med2dev <daniel.bryce.hall@gmail.com>
2021-08-22 10:56:57 -0700

    August 22 challenge

---
+++ cha2021-08-22/app.js
@@ -0,0 +1,12 @@
+// P: two strings, representing a beast and a dish for a feast
+// R: Boolean, are the first and last letters of the beast the same as
+//      the first and last letters of the dish
+
+// E:
+feast('abc', 'azc') // true
+feast('lion', 'lemon') // true
+feast('zebra', 'taco')  // false
+
+function feast(beast, dish){
+    
+}
