a20715480dfd2f6684f614076566dbacbf76ce0d
med2dev <daniel.bryce.hall@gmail.com>
2021-08-22 11:00:05 -0700

    Implement feast

--- cha2021-08-22/app.js
+++ cha2021-08-22/app.js
@@ -8,5 +8,5 @@ feast('lion', 'lemon') // true
 feast('zebra', 'taco')  // false
 
 function feast(beast, dish){
-    
+    return beast[0] == dish[0] && beast[beast.length - 1] == dish[dish.length - 1]
 }
