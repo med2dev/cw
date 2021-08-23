37d0fab537de50b7dd87e8c8a5ad750f72a5d222
med2dev <daniel.bryce.hall@gmail.com>
2021-08-23 11:03:52 -0700

    August 23 challenge

---
+++ cha2021-08-23/app.js
@@ -0,0 +1,12 @@
+// P: 3 integers, a, b, c, with c a nonnegative integer that defaults to 0
+// R: -1, 0, or 1, based on the following comparisons
+//      c is a margin that defines 'close to'
+//      if a is close to b, return 0
+//      if a > b and not close to b, return 1
+//      if a < b and not close to b, return -1
+
+// E:
+
+function closeCompare(a, b, c = 0){
+
+}
