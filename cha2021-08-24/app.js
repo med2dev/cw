391229243e7256ca1305ab9caad0fa1aa6a01167
med2dev <daniel.bryce.hall@gmail.com>
2021-08-24 11:15:48 -0700

    August 24 challenge

---
+++ cha2021-08-24/app.js
@@ -0,0 +1,11 @@
+// P: an array and a function that returns a boolean based on a single element of
+//      whatever type the array is made of
+// R: boolean, does the function return true for each element of the array
+
+// E:
+all([1, 2, 3], v => v < 4) // true
+all([7, 5, 20], v => v % 2) // false
+
+function all(arr, fun){
+
+}
