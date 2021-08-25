a521a10128598e028f6961605831ce6646f63213
med2dev <daniel.bryce.hall@gmail.com>
2021-08-25 11:25:07 -0700

    August 25 challenge

---
+++ cha2021-08-25/app.js
@@ -0,0 +1,10 @@
+// P: a string that may contain a representation of a number
+// R: boolean, is it a decimal number (e.g., and not an expression)
+
+// E: 
+isDigit('-3.2') // true
+isDigit('-3.2 - 4') // false
+
+function isDigit(){
+
+}
