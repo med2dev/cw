4681ce4d6cf599ac8cb6cf390d53e5b49e155b54
med2dev <daniel.bryce.hall@gmail.com>
2021-08-26 11:44:41 -0700

    August 26 challenge

---
+++ cha2021-08-26/app.js
@@ -0,0 +1,12 @@
+// P: x, an integer or string representation of an integer
+// R: one of two strings, as follows
+//      if x^2 > 1000, return "It's hotter than the sun!!"
+//      otherwise, return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+// E:
+apple('50') // 'It\'s hotter than the sun!!'
+apple(4) // 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+function apple(x){
+
+}
