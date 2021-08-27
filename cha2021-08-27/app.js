643a7b2550f32df8dcbb374782b08ea6c4ae518c
med2dev <daniel.bryce.hall@gmail.com>
2021-08-27 01:47:27 -0700

    August 27 challenge

---
+++ cha2021-08-27/app.js
@@ -0,0 +1,10 @@
+// P: min, max, step, integers 
+// R: an array, from min to max by step
+//      return value may include max if step lands on max
+
+// E:
+generateRange(2, 10, 2) // [2, 4, 6, 8, 10]
+
+function generateRange(min, max, step){
+
+}
