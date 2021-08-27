f9e4b0ba89560f9492dcfb8b1f372614fb7e27b2
med2dev <daniel.bryce.hall@gmail.com>
2021-08-27 02:10:12 -0700

    documentation

--- cha2021-08-27/app.js
+++ cha2021-08-27/app.js
@@ -6,5 +6,14 @@
 generateRange(2, 10, 2) // [2, 4, 6, 8, 10]
 
 function generateRange(min, max, step){
+    // standard range implementation
+    // construct array from the length (automagically truncates to integer)
+    // a range starts with the min (thats 1)
+    // and then has the quotient (max - min) / step additional values
+    
+    // generate the values
+    // the first one is min
+    // then add step 
+    // to do this, use min + i * step
     return Array.from({length: (max - min) / step + 1}, (v, i) => min + i*step)
 }
