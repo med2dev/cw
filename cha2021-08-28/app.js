dfaacf66ce54f9eb0cc7136be6cb7c5988947eb4
med2dev <daniel.bryce.hall@gmail.com>
2021-08-28 02:27:27 -0700

    August 28 challenge

---
+++ cha2021-08-28/app.js
@@ -0,0 +1,11 @@
+// P: cuts, an integer number of cuts in 3 dimensions of a cube
+// R: the number of blocks with at least one exterior facing side
+
+// E:
+countSquares(5) // 152
+countSquares(16) // 1538
+countSquares(23) // 3176
+
+function countSquares(cuts){
+
+}
