1d8fe64dedb880f030fe533c9a3caba02441f1ad
med2dev <daniel.bryce.hall@gmail.com>
2021-08-28 02:39:38 -0700

    examine pattern

--- cha2021-08-28/app.js
+++ cha2021-08-28/app.js
@@ -6,6 +6,17 @@ countSquares(5) // 152
 countSquares(16) // 1538
 countSquares(23) // 3176
 
+// the total number of cubes is (cuts + 1)^3
+// interior cubes arise beginning in the 3rd cut
+// 1 interior cube at 3 cuts
+// 64 interior cubes at 5 cuts
+// 3375 interior cubes at 16 cuts
+// only 2 in any dimension, so if there are 3 cuts there is 1 in the middle, in each
+// if there are 4 cuts, there are 2^3 = 8 in the middle
+// if there are 5 cuts, there are 3^3 = 27 in the middle
+// so, total cubes is (cuts + 1)^3 - (cuts - 1)^3
+
+
 function countSquares(cuts){
 
 }
