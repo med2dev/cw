ce58fa3b6da8a845a11e8bf10797791b473ac660
med2dev <daniel.bryce.hall@gmail.com>
2021-08-29 10:34:07 -0700

    August 29 challenge

---
+++ cha2021-08-29/app.js
@@ -0,0 +1,10 @@
+// P: an array of length > 0
+// R: an array containing the elements of the input that are
+//      multiples of their index in the original array
+
+// E:
+multipleOfIndex([4, 3, 2, 7]) // [3, 2]
+
+function multipleOfIndex(arr){
+
+}
