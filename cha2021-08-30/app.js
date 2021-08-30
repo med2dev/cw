9e07e4ed2d4830a4bd88d97a9d9d4a10b02eac07
med2dev <daniel.bryce.hall@gmail.com>
2021-08-30 15:01:57 -0700

    August 30 challenge

---
+++ cha2021-08-30/app.js
@@ -0,0 +1,12 @@
+// P: a string and a substring of that string (typically a single char)
+// R: integer, the number of matches of substring in string
+
+// E:
+strCount('hello', 'o') // 0
+strCount('hello', 'l') // 2
+
+function strCount(string, substring){
+    const pattern = new RegExp(substring, 'g')
+    const matches = string.matches(substring)
+    return match ? matches.length : 0
+}
