dff34a6d94b6741ebdda55eb048df0128634034b
med2dev <daniel.bryce.hall@gmail.com>
2021-08-30 22:57:16 -0700

    non regex implementation

--- cha2021-08-30/app.js
+++ cha2021-08-30/app.js
@@ -12,3 +12,11 @@ function strCount(string, substring){
 }
 
 // TODO: solve without regex
+
+function strCount2(string, letter){
+    // split the string by the letter
+    // the number of letters is the length of the array - 1
+    return string.split(letter).length - 1
+    // yes, this works with the beginning and ending character
+    // split will leave an empty string
+}
