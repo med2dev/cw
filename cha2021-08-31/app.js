a85877f0f6b2429b84790cc7e763c81244a5f354
med2dev <daniel.bryce.hall@gmail.com>
2021-08-31 23:08:34 -0700

    example, then implement

--- cha2021-08-31/app.js
+++ cha2021-08-31/app.js
@@ -4,4 +4,12 @@
 //      note: do not change the input
 
 // E:
+twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]) // 'b***i***t***c***o***i***n'
+twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]) // 'a***r***e'
 
+function twoSort(s){
+    let copy = [...s]
+    const sorted = copy.sort()
+    let first = sorted[0]
+    return first.split('').join('***')
+}
