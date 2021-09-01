565d29a672580a448e6cbaf8b46787bba7c12402
med2dev <daniel.bryce.hall@gmail.com>
2021-08-31 23:07:16 -0700

    August 31 challenge

---
+++ cha2021-08-31/app.js
@@ -0,0 +1,7 @@
+// P: array of strings
+// R: the first element, when the array is sorted alphabetically, with 
+//      *** interpolated between each letter
+//      note: do not change the input
+
+// E:
+
