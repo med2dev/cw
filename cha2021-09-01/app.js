4f4843540f73383dffb1e5a1a540acefc4815104
med2dev <daniel.bryce.hall@gmail.com>
2021-09-01 23:23:22 -0700

    September 1 challenge

---
+++ cha2021-09-01/app.js
@@ -0,0 +1,11 @@
+// P: a nested array representing the steps a monk has climbed every day for a year, 
+//      nested by day of the week
+// R: the sum of all the steps climbed in a year * 20 (representing expectation 
+//      of the number of steps climbed in 20 years)
+
+// E:
+stairsIn20([[1, 2], [3, 4], [5, 6]]) // 21
+
+function stairsIn20(steps){
+
+}
