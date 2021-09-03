3d2c71d8edd9264ddabfb1f7cbbc7c0d6abab0b0
med2dev <daniel.bryce.hall@gmail.com>
2021-09-02 23:31:51 -0700

    September 2 challenge

---
+++ cha2021-09-02/app.js
@@ -0,0 +1,10 @@
+// P: a string, that may end in a '?'
+// R: the string, with a '?' appended, if one isn't there already
+
+// E:
+ensureQuestion('hello world') // 'hello world?'
+ensureQuestion('hello world?') // 'hello world?'
+
+function ensureQuestion(s){
+
+}
