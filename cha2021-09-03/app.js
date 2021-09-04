2a4e2821632d8aeb0bd42e2bb2b5118c2f219745
med2dev <daniel.bryce.hall@gmail.com>
2021-09-03 23:37:50 -0700

    debug

---
+++ cha2021-09-03/app.js
@@ -0,0 +1,11 @@
+// P: debug challenge
+
+// fix the following
+
+function greet(name){
+    if(name === "Johnny"){
+        return 'Hello, my love!'
+    } else {
+        return "Hello, " + name + "!";
+    }
+  }
