a21ba86fc43b31d192f9716d2b7cb18e91c9dd72
med2dev <daniel.bryce.hall@gmail.com>
2021-09-06 09:27:17 -0700

    September 6 challenge

---
+++ cha2021-09-06/app.js
@@ -0,0 +1,14 @@
+// P: string
+
+// R: boolean, is a substring of the input one of 7 greetings in different languages
+//      case insensitive
+//  greetings: 'hello', 'ciao', 'salut', 'hallo', 'hola', 'ajoh', 'czesc'
+
+
+// R:
+validateHello('ajoh') // true
+validateHello('bip') // false
+
+function validateHello(greeting){
+
+}
