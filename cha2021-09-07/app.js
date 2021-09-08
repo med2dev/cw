150272973f8ecc77903a7ca43d765496eb722da3
med2dev <daniel.bryce.hall@gmail.com>
2021-09-07 22:17:52 -0700

    September 7 challenge

---
+++ cha2021-09-07/app.js
@@ -0,0 +1,14 @@
+// P: an integer, n
+// R: a string containing the multiplication table for n, in the following form
+// 1 * n = n
+// 2 * n = 2n
+// 3 * n = 3n ... so on through 10 * n
+// cases where n, 2n, 3n, etc appear in the example above should be replaced by their value
+
+// E:
+multiTable(5)
+// '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50')
+
+function multiTable(n){
+
+}
