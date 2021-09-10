ac5ca89e0f5ee9b6fbdd3f2ba7bdb42110b26a8b
med2dev <daniel.bryce.hall@gmail.com>
2021-09-10 14:17:03 -0700

    September 10 challenge

---
+++ cha2021-09-10/app.js
@@ -0,0 +1,13 @@
+// P: two positive integers, representing the year someone was/will be born
+//      and the year of interest
+// R: a string, describing how old someone will be, or in how many years
+//      they will be born, as follows:
+
+// E:
+calculateAge(2012, 2016) // "You are 4 years old."
+calculateAge(2000, 1990) // "You will be born in 10 years."
+calculateAge(2004, 2004) // "You were born this very year!"
+
+function calculateAge(born, yoi){
+
+}
