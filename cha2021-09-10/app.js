0de705f3e04ef38e627c0aef09bd38993cb66370
med2dev <daniel.bryce.hall@gmail.com>
2021-09-10 14:23:40 -0700

    debug

--- cha2021-09-10/app.js
+++ cha2021-09-10/app.js
@@ -10,5 +10,6 @@ calculateAge(2004, 2004) // "You were born this very year!"
 
 function calculateAge(born, yoi){
     const diff = yoi - born
-    return diff > 0 ? `You are ${diff} years old`: diff < 0 ? `You will be born in ${diff} years.`: `You were born this very year!`
+    const whichYear = Math.abs(diff) == 1 ? 'year' : 'years'
+    return diff > 0 ? `You are ${diff} ${whichYear} old.`: diff < 0 ? `You will be born in ${-1 * diff} ${whichYear}.`: `You were born this very year!`
 }
