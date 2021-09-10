7871c3d19dd1655ad20ba4efce1d79664beeab3c
med2dev <daniel.bryce.hall@gmail.com>
2021-09-10 14:20:38 -0700

    Implement calculateAge

--- cha2021-09-10/app.js
+++ cha2021-09-10/app.js
@@ -9,5 +9,6 @@ calculateAge(2000, 1990) // "You will be born in 10 years."
 calculateAge(2004, 2004) // "You were born this very year!"
 
 function calculateAge(born, yoi){
-
+    const diff = yoi - born
+    return diff > 0 ? `You are ${diff} years old`: diff < 0 ? `You will be born in ${diff} years.`: `You were born this very year!`
 }
