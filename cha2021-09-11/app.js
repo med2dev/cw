e3ce73f731550e0080dbcdc607ee8ae7770bee48
med2dev <daniel.bryce.hall@gmail.com>
2021-09-11 14:42:18 -0700

    Implement distinct

--- cha2021-09-11/app.js
+++ cha2021-09-11/app.js
@@ -7,5 +7,9 @@ distinct([1, 2, 2]) // [1, 2]
 distinct([2, 1, 4]) // [2, 1, 4]
 
 function distinct(arr){
-
+    const res = []
+    for (i = 0; i < arr.length; ++i){
+        if (! res.includes(arr[i])) arr.push(arr[i])
+    }
+    return res
 }
