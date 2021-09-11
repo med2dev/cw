a2e83274f905cc434b6a1350e75bd48db0aceae2
med2dev <daniel.bryce.hall@gmail.com>
2021-09-11 14:37:31 -0700

    September 11 challenge

---
+++ cha2021-09-11/app.js
@@ -0,0 +1,11 @@
+// P: arr, an array
+// R: the array, with duplicates removed, in the same order as the input
+//      note: this means you can't go by way of a set
+
+// E:
+distinct([1, 2, 2]) // [1, 2]
+distinct([2, 1, 4]) // [2, 1, 4]
+
+function distinct(arr){
+
+}
