6fbd267d091a2dc81fd8ab8c283059afc72c2833
med2dev <daniel.bryce.hall@gmail.com>
2021-09-13 22:29:34 -0700

    debug

--- cha2021-09-13/app.js
+++ cha2021-09-13/app.js
@@ -1,10 +1,10 @@
 // P: array of strings, name, and two strings city, and state
-// R:  Madlibs greetin, "Hello <name>! Welcome to <city>, <state>!"
+// R:  Madlibs greetin, "Hello, <name>! Welcome to <city>, <state>!"
 
 // E:
 sayHello(['John', 'Smith'], 'Phoenix', 'Arizona') // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
 
 
 function sayHello(name, city, state){
-    return `Hello ${name.join(' ')}! Welcome to ${city}, ${state}`
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
 }
