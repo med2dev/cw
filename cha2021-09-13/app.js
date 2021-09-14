6038459e4ee263394a11b93ffc7794d84498c4f3
med2dev <daniel.bryce.hall@gmail.com>
2021-09-13 22:25:29 -0700

    September 13 challenge

---
+++ cha2021-09-13/app.js
@@ -0,0 +1,10 @@
+// P: array of strings, name, and two strings city, and state
+// R:  Madlibs greetin, "Hello <name>! Welcome to <city>, <state>!"
+
+// E:
+sayHello(['John', 'Smith'], 'Phoenix', 'Arizona') // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+
+
+function sayHello(name, city, state){
+
+}
