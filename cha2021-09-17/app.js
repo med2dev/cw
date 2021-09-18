ccc2cc0c51e3613852a46d659da6555130921361
med2dev <daniel.bryce.hall@gmail.com>
2021-09-17 23:17:22 -0700

    implement fixTheMeerkat

--- cha2021-09-17/app.js
+++ cha2021-09-17/app.js
@@ -2,7 +2,9 @@
 // R: the array, with the first and last elements swapped
 
 // E:
+fixTheMeerkat(['tail', 'body', 'head']) // ['head', 'body', 'tail']
+
 
 function fixTheMeerkat(arr){
-
+    return arr.reverse()
 }
