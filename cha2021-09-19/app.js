8f7fed29ea2565e16bdf95bc12b1441bd64f5829
med2dev <daniel.bryce.hall@gmail.com>
2021-09-19 22:49:38 -0700

    September 19 challenge

---
+++ cha2021-09-19/app.js
@@ -0,0 +1,11 @@
+// P: string, a name
+// R: string, in the form <name> plays banjo, or <name> does not play banjo
+//      if name begins with R or r, then name plays banjo
+
+// E:
+areYouPlayingBanjo("Adam") // 'Adam does not play banjo'
+areYouPlayingBanjo("Ringo") // 'Ringo plays banjo'
+
+function areYouPlayingBanjo(name){
+
+}
