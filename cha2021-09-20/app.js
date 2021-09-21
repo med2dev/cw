d9a91a103f748b7d304c3879faaab0adc6f05ec3
med2dev <daniel.bryce.hall@gmail.com>
2021-09-20 23:03:17 -0700

    September 20 challenge

---
+++ cha2021-09-20/app.js
@@ -0,0 +1,10 @@
+// P: string, a name
+// R: the number of lightsabers that person names
+//      only Zach owns lightsabers, and he owns 18
+// E:
+howManyLightSabersDoYouOwn('Adam') // 0
+howManyLightSabersDoYouOwn('Zach') // 18
+
+function howManyLightSabersDoYouOwn(name){
+
+}
