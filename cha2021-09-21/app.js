44b6c0550bdf13f9dcac1d1e67a83e1109e623dd
med2dev <daniel.bryce.hall@gmail.com>
2021-09-21 23:20:55 -0700

    September 21 challenge

---
+++ cha2021-09-21/app.js
@@ -0,0 +1,10 @@
+// P: two Point objects, each with x and y attributes
+// R: the distance between the points
+
+// E:
+distancebetweenPoints(new Point(3, 3), new Point(3, 3)) //0
+distancebetweenPoints(new Point(1, 6), new Point(4, 2)) // 5
+
+function distancebetweenPoints(a, b){
+
+}
