cbfe89ba570729b44493e3da4ec3b3f108dc75d2
med2dev <daniel.bryce.hall@gmail.com>
2021-09-21 23:24:47 -0700

    Implement distance

--- cha2021-09-21/app.js
+++ cha2021-09-21/app.js
@@ -6,5 +6,7 @@ distancebetweenPoints(new Point(3, 3), new Point(3, 3)) //0
 distancebetweenPoints(new Point(1, 6), new Point(4, 2)) // 5
 
 function distancebetweenPoints(a, b){
-
+    const xDistance = Math.abs(a.x - b.x)
+    const yDistance = Math.abs(a.y - b.y)
+    return Number(Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2)).toFixed(6))
 }
