416d03d399a75b31432d522e77ceb9141b7bb543
med2dev <daniel.bryce.hall@gmail.com>
2021-09-23 23:38:29 -0700

    set default n = 1

--- cha2021-09-23/app.js
+++ cha2021-09-23/app.js
@@ -1,10 +1,11 @@
 // P: an array, arr, and nonnegative integer, n
 // R: the first n elements of arr
+//      note: set default value for n to 1
 
 // E:
 first([a, b, c], 0) // []
 first([a, b, c], 2) // [a, b]
 
-function first(arr, n){
+function first(arr, n = 1){
     return arr.slice(0, n)
 }
