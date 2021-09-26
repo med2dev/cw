802cba0dec27aa85be08ddb059fedf14a30cce77
med2dev <daniel.bryce.hall@gmail.com>
2021-09-25 23:50:26 -0700

    September 25 challenge

---
+++ cha2021-09-25/app.js
@@ -0,0 +1,10 @@
+// P: two integers, quantity and price of mangoes
+// R: the cost for that quantitty of mangoes, considering that there is a 3 for 2 offer
+
+// E:
+mango(3, 3) // 6
+mango(9, 5) // 30
+
+function mango(quantity, price){
+
+}
