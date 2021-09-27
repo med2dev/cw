9a3cf4e93e3b4c228d3c75acef73420d4ae2bf27
med2dev <daniel.bryce.hall@gmail.com>
2021-09-26 21:15:41 -0700

    September 26 challenge

---
+++ cha2021-09-26/app.js
@@ -0,0 +1,10 @@
+// P: nonnegative integers, h, m, s, representing hours, minutes, and seconds
+// R: integer, the number of milliseconds since midnight
+
+// E:
+past(0, 1, 1) // 61000
+past(1, 0, 0) // 3600000
+
+function past(h, m, s){
+
+}
