d4186f98c6360d87ce805e930faf9a63ebc2ea0a
med2dev <daniel.bryce.hall@gmail.com>
2021-10-01 21:58:36 -0700

    October 1 challenge

---
+++ cha2021-10-01/app.js
@@ -0,0 +1,12 @@
+// P: two integers
+//      position, the position of a player on the board
+//      roll, the dice roll
+
+// R: player position after the move. Each dice roll moves 2 spaces
+move(0, 4) // 8
+move(3, 6) // 15
+move(2, 5) // 12
+
+function move(position, roll){
+    
+}
