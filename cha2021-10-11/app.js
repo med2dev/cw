e6fa71fda4b50073d329d7c2cd60cba52a0ca92e
med2dev <daniel.bryce.hall@gmail.com>
2021-10-11 11:42:23 -0700

    debug

--- cha2021-10-11/app.js
+++ cha2021-10-11/app.js
@@ -1,5 +1,6 @@
 // P: beginner debug assignment challenge
+// this is assignment vs equality test
 
-var a == "code";
-var b == "wa.rs";
-var name == a + b;
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
