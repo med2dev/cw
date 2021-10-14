2c5158d19a347217b28c6a4fa57875b08c0ec2c7
med2dev <daniel.bryce.hall@gmail.com>
2021-10-14 14:34:23 -0700

    October 14 challenge

---
+++ cha2021-10-14/app.js
@@ -0,0 +1,15 @@
+// P: string, containing errors as follows: 
+//      '5' where 'S' should be
+//      '0' where 'O' should be
+//      '1' where 'I' should be
+
+// R: a string with the errors fixed
+
+// E:
+correct("L0ND0N") // "LONDON"
+correct("DUBL1N") // "DUBLIN"
+correnct("51NGAP0RE") // "SINGAPORE"
+
+function correct(string){
+       
+}
