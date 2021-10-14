1da7a9239642c3220ab5c482da5a5a2dc3968d88
med2dev <daniel.bryce.hall@gmail.com>
2021-10-14 16:18:07 -0700

    Implement translation and correct functions

--- cha2021-10-14/app.js
+++ cha2021-10-14/app.js
@@ -10,6 +10,14 @@ correct("L0ND0N") // "LONDON"
 correct("DUBL1N") // "DUBLIN"
 correnct("51NGAP0RE") // "SINGAPORE"
 
+// 3 functions
+const trans5toS = s => s.replace(/5/g, 'S')
+const trans0toO = s => s.replace(/0/g, 'O')
+const trans1toI = s => s.replace(/1/g, 'I')
+
 function correct(string){
-       
+    // 5 to S
+    const string5toS = trans5toS(string)
+    const string0toO = trans0toO(string5toS)
+    return trans1toI(string0toO)
 }
