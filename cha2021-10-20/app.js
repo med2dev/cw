b27d564b5256080e0a87ec4a87a8ccfa17c36142
med2dev <daniel.bryce.hall@gmail.com>
2021-10-20 00:08:41 -0700

    October 20 challenge

---
+++ cha2021-10-20/app.js
@@ -0,0 +1,12 @@
+// P: two positive integers, the age of a father and son
+// R: the number of years ago the father was twice as old as his son
+//      or, in how many years
+
+// E:  
+twiceAsOld(55, 30) // 5
+twiceAsOld(42, 21) // 0
+twiceAsOld(29, 0) // 29
+
+function twiceAsOld(dad, son){
+
+}
