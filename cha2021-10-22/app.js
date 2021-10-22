9a4960251c5b0fd21b3168ee5327b99d83e4b992
med2dev <daniel.bryce.hall@gmail.com>
2021-10-22 00:26:32 -0700

    October 22 challenge

---
+++ cha2021-10-22/app.js
@@ -0,0 +1,12 @@
+// P: two floats, weight, height
+// R: category of body mass index, where
+//      bmi = weight / height*height
+//      bmi  > 30:           'Obese'
+//      25   < bmi <= 30:    'Overweight'
+//      18.5 < bmi <= 25:    'Normal'
+//             bmi <= 18.5:  'Underweight'
+bmi(80, 1.8) // 'Normal'
+
+function bmi(weight, height){
+
+}
