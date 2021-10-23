e04a5b45a73fa3c43bdee5388b6346f7dcfc9911
med2dev <daniel.bryce.hall@gmail.com>
2021-10-23 01:00:07 -0700

    handle mixed type

--- cha2021-10-23/app.js
+++ cha2021-10-23/app.js
@@ -2,9 +2,15 @@
 // R: the sum of the true values
 
 // E:
-countSheeps([true, false, true, null]) // 2
+countSheep([true, false, true, null]) // 2
 
-function countSheeps(arr){
+function countSheep(arr){
     // if array is null or empty, return 0
     return arr ? arr.reduce((p, c) => p + c) : 0
+}
+
+// handle other types, but still only return the number of true
+
+function countSheep2(arr){
+    return arr ? arr.reduce((p,c) => typeof(c) == 'boolean' ? p + c : p + 0) : 0
 }
