9d348ab838418468b11fbe0041b8275a0350a247
med2dev <daniel.bryce.hall@gmail.com>
2021-10-25 23:05:21 -0700

    October 25 challenge

---
+++ cha2021-10-25/app.js
@@ -0,0 +1,12 @@
+// P: a two dimensional array
+// R: string representation of that 2d array
+// E:
+toCsvText([[0, 1, 2, 3, 4],
+ [10, 11, 12, 13, 14]])
+// '0,1,2,3,4\n10,11,12,13,14'
+
+
+
+function toCsvText(array){
+
+}
