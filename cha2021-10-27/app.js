cbc6a93493da0871f796cf17db9bbbfe010d4d49
med2dev <daniel.bryce.hall@gmail.com>
2021-10-27 21:57:34 -0700

    October 27 challenge

---
+++ cha2021-10-27/app.js
@@ -0,0 +1,11 @@
+// P:  three strings, operation, value1, value2
+//      operation is one of '+', '-', '*', '/'
+//      value1 and value2 are numbers, the operands
+//      for the operation
+
+// R: the value of the operation
+basicOp('+', 4, 7) // 11
+
+function basicOp(operation, value1, value2){
+
+}
