239c4bf517f1c70beb5c2c57533468e5a3a0ff21
med2dev <daniel.bryce.hall@gmail.com>
2021-10-27 22:02:23 -0700

    switch implementation

--- cha2021-10-27/app.js
+++ cha2021-10-27/app.js
@@ -12,3 +12,11 @@ function basicOp(operation, value1, value2){
     return eval(`${value1} ${operation} ${value2}`)
 }
 
+function basicOpSafe(operation, value1, value2){
+    switch (operation){
+        case '+': return value1 + value2
+        case '-': return value1 - value2
+        case '*': return value1 * value2
+        case '/': return value1 / value2
+    }
+}
