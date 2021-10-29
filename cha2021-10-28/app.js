a8cc96eb8bd4aa91f06b6c252672f85b8f18b142
med2dev <daniel.bryce.hall@gmail.com>
2021-10-28 22:16:06 -0700

    October 28 challenge

---
+++ cha2021-10-28/app.js
@@ -0,0 +1,14 @@
+// P: an array of integers
+// R: an array, the two unique highest values
+//    in the event there are not two unique highest values, return as many as
+//      there are
+
+// E: 
+twoHighest([4, 10, 10, 9]) // [10, 9]
+twoHighest([1, 1, 1]) // [1]
+twoHighest([]) // []
+
+function twoHighest(arr){
+
+}
+
