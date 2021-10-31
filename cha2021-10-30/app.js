92ce1f6c1ab87bfce7cce86127cdfb6f7b1e46b0
med2dev <daniel.bryce.hall@gmail.com>
2021-10-30 22:50:05 -0700

    Implement squareSum

--- cha2021-10-30/app.js
+++ cha2021-10-30/app.js
@@ -6,5 +6,7 @@ squareSum([1, 2, 2]) // 9
 squareSum([0, 3, 4, 5]) // 50
 
 function squareSum(numbers){
-
+    // reduce
+    // initial value is 0, so that you only square current
+    return numbers.reduce((p, c) => p + Math.pow(c, 2), 0)
 }
