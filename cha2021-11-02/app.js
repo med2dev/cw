d70e60714f0f91f77de247f1f183d96b435c207c
med2dev <daniel.bryce.hall@gmail.com>
2021-11-02 01:12:30 -0700

    documentation

--- cha2021-11-02/app.js
+++ cha2021-11-02/app.js
@@ -5,5 +5,7 @@
 arrayPlusArray([1, 2, 3], [4, 5, 6]) // 21
 
 function arrayPlusArray(arr1, arr2){
+    // spread syntax, pass both to reduce
+    // reduce idiom to sum
     return [...arr1, ...arr2].reduce((p, c) => p + c)
 }
