685e5643ad6800edf4652df95f10c0545feada28
med2dev <daniel.bryce.hall@gmail.com>
2021-11-03 01:34:48 -0700

    documentation

--- cha2021-11-03/app.js
+++ cha2021-11-03/app.js
@@ -8,6 +8,9 @@ isVow([118, 117]) // [118, 'u']
 
 function isVow(a){
     // P: map
+    // define an array containing code points for lowercase vowels
     const vowelPoints = [97, 101, 105, 111, 117]
+    // map each element of the array, if it's a vowel code point, to the vowel,
+    //      otherwise, leave it alone
     return a.map((e, i, a) => vowelPoints.includes(e) ? String.fromCodePoint(e) : e)
 }
