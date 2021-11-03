34c7942c2fca2810e274d830e88332fd8ff45af0
med2dev <daniel.bryce.hall@gmail.com>
2021-11-03 01:32:55 -0700

    implement isVow

--- cha2021-11-03/app.js
+++ cha2021-11-03/app.js
@@ -8,5 +8,6 @@ isVow([118, 117]) // [118, 'u']
 
 function isVow(a){
     // P: map
-    
+    const vowelPoints = [97, 101, 105, 111, 117]
+    return a.map((e, i, a) => vowelPoints.includes(e) ? String.fromCodePoint(e) : e)
 }
