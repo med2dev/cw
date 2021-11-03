057152de7e504c6dc2399a95383f28177cf73079
med2dev <daniel.bryce.hall@gmail.com>
2021-11-03 01:22:53 -0700

    November 3 challenge

---
+++ cha2021-11-03/app.js
@@ -0,0 +1,12 @@
+// P: an array of numbers
+// R: an array, with those values that are character codes for 
+//      lowercase vowels converted to lowercase vowels
+
+// E:
+isVow([118, 117]) // [118, 'u']
+
+
+function isVow(a){
+    // P: map
+    
+}
