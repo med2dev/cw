1348800fc4727d69e581ea5b674175cee4e5b0cc
med2dev <daniel.bryce.hall@gmail.com>
2021-11-04 01:37:29 -0700

    November 4 challenge

---
+++ cha2021-11-04/app.js
@@ -0,0 +1,11 @@
+// P: an array, a, and a value, x
+// R: Boolean, whether the array contains the value x
+
+// E:
+check([66, 101], 66) // true
+check([101, 45, 75], 107) // false
+check(['t', 'e', 's', 't'], 'e') // true
+
+function check(a, x){
+
+}
