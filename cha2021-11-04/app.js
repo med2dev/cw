d8746c8f70f342f6936b05fd359771db1c620dfa
med2dev <daniel.bryce.hall@gmail.com>
2021-11-04 01:39:15 -0700

    implement check

--- cha2021-11-04/app.js
+++ cha2021-11-04/app.js
@@ -6,6 +6,7 @@ check([66, 101], 66) // true
 check([101, 45, 75], 107) // false
 check(['t', 'e', 's', 't'], 'e') // true
 
+// this is implemented as Array.prototype.includes()
 function check(a, x){
-
+    return a.includes(x)
 }
