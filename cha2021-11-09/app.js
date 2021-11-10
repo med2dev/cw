5f0cf3522d53a3e9a77368f651fc3b279d65436a
med2dev <daniel.bryce.hall@gmail.com>
2021-11-09 23:36:43 -0700

    refactor

--- cha2021-11-09/app.js
+++ cha2021-11-09/app.js
@@ -7,5 +7,15 @@ summation(8) // 36
 
 
 function summation(n){
+    // P:   sum of consecutive integers is 
+    //      n(last + first)/2
+    // here: n(n+1)/2
     return n * (n + 1) / 2
+}
+
+// actual sum implementation
+
+function fullSummation(n){
+    const arr = Array.from({length:n}, (v,i) => i + 1)
+    return arr.reduce((p, c) => p + c, 0)
 }
