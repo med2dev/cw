f46f9d1e5516d2e12935829f0c802a8de61a520f
med2dev <daniel.bryce.hall@gmail.com>
2021-11-10 23:43:29 -0700

    November 10 challenge

---
+++ cha2021-11-10/app.js
@@ -0,0 +1,10 @@
+// P: string
+// R: length of the longest word, where a word is defined as a run of characters 
+//      separated by spaces
+
+// E: 
+findLongest("The quick white fox jumped around the massive dog") // 7
+
+function findLongest(str){
+
+}
