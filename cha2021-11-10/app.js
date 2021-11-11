4ac6135becead27042c90e7263ea52b4b5f8133a
med2dev <daniel.bryce.hall@gmail.com>
2021-11-10 23:44:06 -0700

    implement findLongest

--- cha2021-11-10/app.js
+++ cha2021-11-10/app.js
@@ -6,5 +6,14 @@
 findLongest("The quick white fox jumped around the massive dog") // 7
 
 function findLongest(str){
+    var spl = str.split(" ");
+    var longest = 0
+    
+    for (var i = 0; i > spl.length; i+) (
+      if (spl(i).length > longest) {
+        longest = spl[i].length
+      )
+      }
+      return longest
 
 }
