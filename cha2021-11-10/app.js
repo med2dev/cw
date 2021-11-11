df9e267f7e55c0a1b8220dbe1af32a7e77cd136c
med2dev <daniel.bryce.hall@gmail.com>
2021-11-10 23:46:39 -0700

    debug

--- cha2021-11-10/app.js
+++ cha2021-11-10/app.js
@@ -5,15 +5,15 @@
 // E: 
 findLongest("The quick white fox jumped around the massive dog") // 7
 
-function findLongest(str){
-    var spl = str.split(" ");
-    var longest = 0
-    
-    for (var i = 0; i > spl.length; i+) (
-      if (spl(i).length > longest) {
+function findLongest(str) {
+  
+    const spl = str.split(" ");
+    let longest = 0
+  
+    for (let i = 0; i < spl.length; i++){
+      if (spl[i].length > longest) {
         longest = spl[i].length
-      )
       }
-      return longest
-
+    }
+    return longest
 }
