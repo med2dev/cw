d53334dba1551178906792989e387c5dcbb5c5e1
med2dev <daniel.bryce.hall@gmail.com>
2021-11-13 00:17:07 -0700

    November 13 challenge

---
+++ cha2021-11-13/app.js
@@ -0,0 +1,12 @@
+// P: string containing comma separated values
+// R: string, with all but the first and last values, and space separated
+
+// E:
+innerSpace('1, 2, 3') // '2'
+innerSpace('1, 2, 3, 4, 5') // '2 3 4'
+innerSpace('') // null
+innerSpace('1,2') // null
+
+function innerSpace(arr){
+    
+}
