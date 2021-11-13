f71cd5c0fa8170e28a396bb2b19c9776e870d1eb
med2dev <daniel.bryce.hall@gmail.com>
2021-11-13 00:22:33 -0700

    implement innerSpace

--- cha2021-11-13/app.js
+++ cha2021-11-13/app.js
@@ -2,11 +2,11 @@
 // R: string, with all but the first and last values, and space separated
 
 // E:
-innerSpace('1, 2, 3') // '2'
-innerSpace('1, 2, 3, 4, 5') // '2 3 4'
+innerSpace('1,2,3') // '2'
+innerSpace('1,2,3,4,5') // '2 3 4'
 innerSpace('') // null
 innerSpace('1,2') // null
 
 function innerSpace(arr){
-    
+    return arr.length > 3 ? arr.slice(2,-2).split(',').join(' ') : null
 }
