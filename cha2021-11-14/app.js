193578e2c6b9c8a12932483246195c58386518df
med2dev <daniel.bryce.hall@gmail.com>
2021-11-14 13:38:05 -0700

    Novemeber 14 challenge

---
+++ cha2021-11-14/app.js
@@ -0,0 +1,12 @@
+// P: float, representing a time in hours
+// R: integer, representing an amount of liters of water drunk
+//      specs say: 0.5 liters of water per hour, rounded to the smallest value
+//      from examples, it appears this means specs require floor
+
+// E:
+liters(3) // 1
+liters(1.4) // 0  (from this example, it is apparent we need a floor function)
+
+function liters(time) {
+    return 
+}
