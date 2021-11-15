636e562c6ec8fc876326a188e0f90885604ad262
med2dev <daniel.bryce.hall@gmail.com>
2021-11-15 13:42:52 -0700

    documentation

--- cha2021-11-15/app.js
+++ cha2021-11-15/app.js
@@ -5,16 +5,19 @@
 updateLight('green') // 'yellow'
 
 function updateLight(current){
+    // create an object with key: current color, value: next color
     const nextLight = {
         green: 'yellow',
         yellow: 'red', 
         red: 'green'
     }
+    // index object to get the next collor
     return  nextLight[current]
 }
 
 // logic solution
 
 function updateLightLogic(current){
+    // if it's green make it yellow, if not, is it yellow? make it red. if not, make it green
     return current == 'green' ? 'yellow' : current = 'yellow' ? 'red' : 'green'
 }
