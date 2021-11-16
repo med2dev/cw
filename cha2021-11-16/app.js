d8f691c9de110069d5aa10cd05a777ff7d002582
med2dev <daniel.bryce.hall@gmail.com>
2021-11-16 13:45:37 -0700

    documentation

--- cha2021-11-16/app.js
+++ cha2021-11-16/app.js
@@ -5,5 +5,10 @@
 checkForFactor(10, 2) // true
 
 function checkForFactor(base, factor){
+    // logical test
+    // if the remainder of the base divided by the factor is 0
+    // then factor is in fact a factor of base
+    // 0 is falsey. 
+    // so invert
     return ! (base % factor)
 }
