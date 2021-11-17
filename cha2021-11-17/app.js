0db87d05f4de3b1a960f829187c5574109fb9748
med2dev <daniel.bryce.hall@gmail.com>
2021-11-17 00:39:35 -0700

    white space

--- cha2021-11-17/app.js
+++ cha2021-11-17/app.js
@@ -10,5 +10,5 @@ quarterOf(12) // 4
 function quarterOf(m){
     // P: no need to go through the months. This is just a simple math problem
     // x/12 = y/4, so y = x/3
-    return Math.ceil(m/3)
+    return Math.ceil(m/3) 
 }
