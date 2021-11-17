b41915973c5964e5793d1e66e25394bd8e6d6026
med2dev <daniel.bryce.hall@gmail.com>
2021-11-17 13:50:38 -0700

    documentation

--- cha2021-11-17/app.js
+++ cha2021-11-17/app.js
@@ -10,5 +10,6 @@ quarterOf(12) // 4
 function quarterOf(m){
     // P: no need to go through the months. This is just a simple math problem
     // x/12 = y/4, so y = x/3
+    // ceil because, e.g., 1/3, 2/3, 3/3 are all in the first quarter
     return Math.ceil(m/3) 
 }
