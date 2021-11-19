6dd1f02690f3ecaa6586dbc3ca8b35da707b1dd7
med2dev <daniel.bryce.hall@gmail.com>
2021-11-19 13:54:31 -0700

    documentation

--- cha2021-11-19/app.js
+++ cha2021-11-19/app.js
@@ -7,6 +7,12 @@ enough(10, 5, 5)    // 0
 enough(100, 60, 50) // 10
 
 function enough(cap, on, wait){
+    // the number of people left is the number of people on the bus
+    // plus the number of people waiting to get on the bus
+    // minus the capacity
     const nextBus = on + wait - cap
+    // if the capacity is greater than the number waiting + on
+    // this will be negative, in which case, 0 people will have to
+    // stay at the bus stop and wait for the next bus
     return nextBus < 0 ? 0 : nextBus
 }
