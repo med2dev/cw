2d29614d67957cabba0f107a44e8f62402e897bb
med2dev <daniel.bryce.hall@gmail.com>
2021-11-23 14:07:31 -0700

    documentation

--- cha2021-11-23/app.js
+++ cha2021-11-23/app.js
@@ -1,5 +1,6 @@
 // P: an integer, num
-// R: the absolute value of the difference between 6 and num
+// R: how many over or under num 6 is
+
 
 // E:
 sixToast(5) // 1
@@ -7,5 +8,7 @@ sixToast(12) // 6
 
 
 function sixToast(num){
+    // distance between num and 6 is 
+    // the absolute value
     return Math.abs(num - 6)
 }
