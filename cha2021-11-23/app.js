6f1675bd5ef751c431dac20d631092067bbd0536
med2dev <daniel.bryce.hall@gmail.com>
2021-11-23 23:53:31 -0700

    November 23 challenge

---
+++ cha2021-11-23/app.js
@@ -0,0 +1,11 @@
+// P: an integer, num
+// R: the absolute value of the difference between 6 and num
+
+// E:
+sixToast(5) // 1
+sixToast(12) // 6
+
+
+function sixToast(num){
+    return Math.abs(num - 6)
+}
