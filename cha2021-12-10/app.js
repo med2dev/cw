21024cd8f470131b0d4e86a8587d85600502695d
med2dev <daniel.bryce.hall@gmail.com>
2021-12-10 18:05:48 -0700

    December 10 challenge

---
+++ cha2021-12-10/app.js
@@ -0,0 +1,8 @@
+// P: an integer, an ascii character value
+// R: the character with that ascii value
+
+// E:
+getChar(65) // 'A'
+
+// this is implemented as a string method
+const getChar = n => String.fromCharCode(n)
