9f87c554725c5a81075392ce3429c0f9c2e4d824
med2dev <daniel.bryce.hall@gmail.com>
2021-12-13 13:35:40 -0700

    resolved case where m is included

--- cha2021-12-13/app.js
+++ cha2021-12-13/app.js
@@ -4,6 +4,7 @@
 
 // E:
 sumMu(2, 9) // 20
+sumMu(2, 4) // 2
 sumMu(4, -7) // "INVALID"
 
 function sumMu(n, m){
@@ -11,9 +12,11 @@ function sumMu(n, m){
     if (n < 1 || m < 1) return "INVALID"
 
     // generate the multiples
-    // the number of multiples is the quotient m/n
+    // the number of multiples less than or equal to m is the quotient m / n
+    // Here, m isn't included, e.g., n = 2, m = 4
+    // so use m - 1
     // the values are (index + 1) * n
-    const arr = Array.from({ length: Math.floor(m / n) }, (v, i) => (i + 1) * n)
+    const arr = Array.from({ length: Math.floor((m - 1) / n) }, (v, i) => (i + 1) * n)
 
     // sum
     return arr.reduce((p, c) => p + c)
