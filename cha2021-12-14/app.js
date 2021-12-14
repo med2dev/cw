4c2187fa477804a24906d082fb7aecc7d4af6ffc
med2dev <daniel.bryce.hall@gmail.com>
2021-12-14 14:28:45 -0700

    December 14 challenge

---
+++ cha2021-12-14/app.js
@@ -0,0 +1,14 @@
+// P: a string that contains spaces. 
+// R: a string, with the words (space spearated substrings) in reverse order
+
+
+// E: 
+reverseWords('hello world!') // 'world! hello'
+
+function reverseWords(s){
+    // P:
+    // split the string into an array of words
+    const words = s.split(" ")
+    // reverse the array and join it
+    return words.reverse().join(' ')
+}
