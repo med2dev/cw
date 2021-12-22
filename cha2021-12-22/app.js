b8a619164fad0d8694fc9c8a7fc9b78a25547bc8
med2dev <daniel.bryce.hall@gmail.com>
2021-12-22 01:12:17 -0700

    December 22 challenge

---
+++ cha2021-12-22/app.js
@@ -0,0 +1,11 @@
+// P: an integer and a string
+// R: the string, concatenated n times
+
+// E:
+repeatStr(6, "I") // "IIIIII"
+
+
+// this already exists as a string method
+function repeatStr(n, s){
+    return s.repeat(n)
+}
