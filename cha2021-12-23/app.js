62065e7c94749ec1eb95a16389465e3d095409cd
med2dev <daniel.bryce.hall@gmail.com>
2021-12-23 01:16:47 -0700

    December 23 challenge

---
+++ cha2021-12-23/app.js
@@ -0,0 +1,11 @@
+// P: num, a number, appears to be only integers
+
+// R: a string representation of that number
+
+// E:
+numberToString('12') // 12
+
+// this exists as a builtin
+function numberToString(num){
+    return String(num)
+}
