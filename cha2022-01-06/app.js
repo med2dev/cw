ad1d439368faf6e980b92377adb37adc214d2634
med2dev <daniel.bryce.hall@gmail.com>
2022-01-06 18:26:43 -0700

    account for empty argument

--- cha2022-01-06/app.js
+++ cha2022-01-06/app.js
@@ -8,10 +8,13 @@ hello('alice') // 'Hello Alice!'
 
 // name needs to be capitalized. break this into a separate function
 // raise the first character to upper case, concatenate with the rest of the string in lower case
-const capitalize = s => s[0].toUpperCase() + s.substring(1).toLowerCase()
+// account for edge case of s = '' by returning the string itself if s is falsey
+const capitalize = s => s ? s[0].toUpperCase() + s.substring(1).toLowerCase() : s 
+
 
 // pass a default value
 function hello(name = world){
     // template literal
     return `Hello ${capitalize(name)}!`
 }
+
