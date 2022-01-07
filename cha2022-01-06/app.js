b1ee6a3430524e69eac587b12e2dab306646ebb7
med2dev <daniel.bryce.hall@gmail.com>
2022-01-06 17:51:51 -0700

    January 6 challenge

---
+++ cha2022-01-06/app.js
@@ -0,0 +1,17 @@
+// P: string, a name
+// R: string, 'Hello, <transformed name>!', where name is capitalized name
+//      Edge case: if no name is given, return 'Hello, World!'
+
+// E:
+hello() // 'Hello World!'
+hello('alice') // 'Hello Alice!'
+
+// name needs to be capitalized. break this into a separate function
+// raise the first character to upper case, concatenate with the rest of the string in lower case
+const capitalize = s => s[0].toUpperCase() + s.substring(1).toLowerCase()
+
+// pass a default value
+function hello(name = world){
+    // template literal
+    return `Hello ${capitalize(name)}!`
+}
