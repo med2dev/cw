e7120b4746a39e102ee0420c7e57588a8831da49
med2dev <daniel.bryce.hall@gmail.com>
2022-01-08 21:20:55 -0700

    regex added

--- cha2022-01-08/app.js
+++ cha2022-01-08/app.js
@@ -8,3 +8,13 @@ isLockNessMonster('hello world') // false
 function isLockNessMonster(s){
     return (s.includes('tree fiddy') || s.includes('3.50') || s.includes('three fifty')) ? true : false;
 }
+
+function isLockNessRegex(s){
+    return /3\.50|(th?ree fi(ft)|(dd)y)/.test(s)
+}
+
+// regex that matches either:
+//  subexpression: 3.50, with the '.' escaped
+// or, the following subexpression:
+//      th?ree means three or tree (h is optional)
+//      fi(ft)|(dd)y means fifty or fiddy (either ft or dd can take the 3rd and 4th character)
