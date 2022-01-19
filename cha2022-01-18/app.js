3366c7af9b8b41b9db3bf568eacd5af82226ac3d
med2dev <daniel.bryce.hall@gmail.com>
2022-01-18 23:48:51 -0700

    January 18 challenge

---
+++ cha2022-01-18/app.js
@@ -0,0 +1,20 @@
+// PREP
+
+// P:
+// string representation of an integer
+// may include negative numbers
+
+
+// R:
+// integer
+
+// E:
+stringToNumber("1234") // 1234
+stringToNumber("605") // 605
+stringToNumber("1405") // 1405
+stringToNumber("-7") // -7
+
+// Pseudocode:
+// this is implemented in JS as the Number constructor
+
+const stringToNumber = Number
