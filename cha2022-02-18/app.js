0fbf4c566f9890bb98e8e852647874f2b3db4c67
med2dev <daniel.bryce.hall@gmail.com>
2022-02-21 17:15:39 -0700

    cleanup and refactor

--- cha2022-02-18/app.js
+++ cha2022-02-18/app.js
@@ -18,6 +18,11 @@
 // Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
 
 function trueOrFalse(val){
-    if (val)    return 'true';             
-    else     return 'false';
-  }
+    if (val) return 'true'             
+    else return 'false'
+}
+
+// alternate
+function trueOrFalse(val){
+    return val ? 'true' : 'false'
+}
