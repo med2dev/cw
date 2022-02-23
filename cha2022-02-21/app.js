31c6f42e173e1c23f36ba6c66f00195a7d65ce68
med2dev <daniel.bryce.hall@gmail.com>
2022-02-22 22:53:12 -0700

    where Array.prototype.at() is implemented

--- cha2022-02-21/app.js
+++ cha2022-02-21/app.js
@@ -14,3 +14,20 @@ function howMuchILoveYou(n){
     const responses = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
     return responses[(n % 6) - 1] || 'not at all'
 }
+
+// note the || solution 
+// here we want to use a count as an index, where we allow counts > the length of the index to find 
+// the value that would recycle to that position 
+// This is simply solved by using a remainder EXCEPT for languages (like JS) where the index position
+// -1 is undefined
+// the current experimental method Array.prototype.at() resolves this by implementing negative indices
+// But as it is, we need a work around
+// this is the work around. We know if the value at the calculated index is undefined, then we 
+// actually want the last element
+// the idiom return A || B will give us B when A is falsy, and undefined is falsy
+
+// alternate where arr.at() is implemented
+function howMuchILoveYou(n){
+    const responses = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+    return responses.at((n % 6) - 1) // Array.at(-n) returns the element n + 1 from the end
+}
