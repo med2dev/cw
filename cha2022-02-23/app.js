72b5d48671a6d8bf33a14bed5fcfea70f760a749
med2dev <daniel.bryce.hall@gmail.com>
2022-02-23 19:49:36 -0700

    February 23 challenge

---
+++ cha2022-02-23/app.js
@@ -0,0 +1,13 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+// P: integer
+// R: an integer 2x the parameter
+// E:
+doubleInteger(2) // 4
+doubleInteger(0) // 0
+doubleInteger(-5) // -10
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return 2*i;
+  }
