b1a3582abc48bed6dd49ea9b2c6a3a3793310d8f
med2dev <daniel.bryce.hall@gmail.com>
2022-02-26 00:05:18 -0700

    February 26 challenge

---
+++ cha2022-02-26/app.js
@@ -0,0 +1,16 @@
+// P: string containing a binary representation of a number
+// R: integer, decimal representation of the same number
+
+// E:
+binToDec('1') // 1
+binToDec('0') // 0
+binToDec("1001001") // 73
+
+function binToDec(bin){
+    let res = 0
+    bin.split("").reverse().forEach((e,i) => res+= e * 2**i)
+    return res
+}
+
+// note, there is a builtin
+// parseInt
