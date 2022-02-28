4c9d99e782be05e6f9be55c4fdeb3831ccbf0dc8
med2dev <daniel.bryce.hall@gmail.com>
2022-02-27 18:20:17 -0700

    February 27 challenge

---
+++ cha2022-02-27/app.js
@@ -0,0 +1,21 @@
+// A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems confusing, refer to the example below.
+
+// Ex: 153, where l = 3 ( the number of digits in 153 )
+// 13 + 53 + 33 = 153
+
+// Write a function that, given n, returns whether or not n is a Narcissistic Number.
+
+// P: an integer, tests appear to all be positive
+// R: boolean
+// Where p is the number of digits, is the integer, n, equal to the sum of each digit raised to the power p
+
+
+function isNarcissistic(n){
+    const s = String(n)
+    const p = s.length
+    let powerSum = 0
+    for (let i = 0; i < p; i++){
+      powerSum += s[i]**p
+    }
+    return powerSum == n
+}
