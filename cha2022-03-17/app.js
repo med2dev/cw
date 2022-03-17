bc22b28fdd5414300bc182094f348014257a8e94
med2dev <daniel.bryce.hall@gmail.com>
2022-03-17 01:28:39 -0700

    March 17 challenge

---
+++ cha2022-03-17/app.js
@@ -0,0 +1,28 @@
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// ghostBusters("Sky scra per");
+// Should return:
+
+// "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+// P: string, possibly containing white space
+// R: string, with white space removed, unless no white space was present, in which case 'You just wanted my autograph didn't you?"
+
+//E 
+ghostBusters("Factor y") // 'Factory'
+ghostBusters("O   f fi ce") // 'Office'
+ghostBusters("BusStation") // 'You just wanted my autograph didn't you?""
+
+function ghostBusters(building){
+    const result = building.replace(/\s+/g, '')
+    return result == building ? "You just wanted my autograph didn't you?" : result
+}
