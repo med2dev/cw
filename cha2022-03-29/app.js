3b13732ba3ecc641593bba986814059b294c4f9c
med2dev <daniel.bryce.hall@gmail.com>
2022-03-29 13:52:10 -0700

    March 29 challenge

---
+++ cha2022-03-29/app.js
@@ -0,0 +1,12 @@
+// P: no parameters, write this as a string method
+// R: logical, are all the letters in the string upper case
+
+// E:
+'hello WORLD'.isUpperCase() // false
+'HELLO WORLD'.isUpperCase() // true
+'8 43-?6**'.usUpperCase() // true
+
+String.prototype.isUpperCase = function() {
+    // P: change to upper case, compare
+    return this.toUpperCase() == this
+}
