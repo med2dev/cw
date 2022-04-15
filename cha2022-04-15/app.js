8281a36aaec5bb38dae10b36f5d7c68161416f86
med2dev <daniel.bryce.hall@gmail.com>
2022-04-15 09:41:07 -0700

    refactor

--- cha2022-04-15/app.js
+++ cha2022-04-15/app.js
@@ -19,3 +19,10 @@ function isToday(d){
     return today.getDate() == d.getDate() && today.getMonth() == d.getMonth() && today.getFullYear() == d.getFullYear()
 
 }
+
+// more concise.. anything that is today will have the same date string
+
+function isToday(d){
+    const today = new Date()
+    return today.toISOString().substring(0,10) == d.toISOString().substring(0, 10)
+}
