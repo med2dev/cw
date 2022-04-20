165dfaf7015f4e18d4c515574f82aa4378908479
med2dev <daniel.bryce.hall@gmail.com>
2022-04-20 11:48:10 -0700

    April 20 challenge

---
+++ cha2022-04-20/app.js
@@ -0,0 +1,21 @@
+// P:   an array of strings representing types of birds, birds
+// R:   an array of strings those birds that are geese removed
+//      geese:
+//      ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+// E:
+gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", 
+"Toulouse", "Blue Swedish"])
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+function gooseFilter(birds){
+    // finding the elements in one array that are not in another is a simple
+    // set operation. However, to pass the tests, it looks like we'll need to 
+    // retain the order. So it may be best to simply loop through the array.
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    const notgeese = []
+    birds.forEach( element => {
+        if (! geese.includes(element)) notgeese.push(element)
+    })
+    return notgeese
+}
