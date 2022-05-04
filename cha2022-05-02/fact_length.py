62f83b587ef56ecb8219f82209521c698555148c
med2dev <daniel.bryce.hall@gmail.com>
2022-05-03 22:19:21 -0700

    documentation

--- cha2022-05-02/fact_length.py
+++ cha2022-05-02/fact_length.py
@@ -1,3 +1,16 @@
+"""
+Implement a function, count, that takes an integer and returns the number of
+digits in factorial(n).
+
+For example, count(5) = 3, because 5! = 120, and 120 has 3 digits.
+
+More examples in the test cases.
+
+Brute force is not possible. A little research will go a long way, as this is
+a well known series.
+"""
+
+
 from math import ceil, lgamma, log
 
 def count(n):
