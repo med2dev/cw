783de7b6fda1aafe7e72ae6a6c7db3fcc44fb3ac
med2dev <daniel.bryce.hall@gmail.com>
2022-05-17 22:00:09 -0700

    documentation

--- cha2022-05-04/sum_prime.py
+++ cha2022-05-04/sum_prime.py
@@ -1,3 +1,9 @@
+"""
+given an integer array and your task is to return the sum of elements occupying prime-numbered indices.
+
+The first element of the array is at index 0.
+"""
+
 def is_prime(n):
     if n < 3: return n == 2
     if not n%2: return False
