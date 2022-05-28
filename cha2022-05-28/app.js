e31c04eecf4ac93c337621686def46271adb244d
med2dev <daniel.bryce.hall@gmail.com>
2022-05-28 13:07:27 -0700

    Fixed documentation error, added max

--- cha2022-05-28/app.js
+++ cha2022-05-28/app.js
@@ -1,8 +1,20 @@
 // P: an array of integers
 // R: the minimum value
 
+const { maxHeaderSize } = require("http")
+
 // E: 
-min([-2, 4, 2, -7, 3]) // -3
+min([-2, 4, 2, -7, 3]) // -7
 
 // P: this is a builtin Math method, you just need to use spread notation
 const min = arr => Math.min(...arr)
+
+// Max works exactly the same
+// P: an array of integers
+// R: the maximum value
+
+
+// E:
+max([-2, 4, 2, -7, 3]) // 3
+
+const max = arr => Math.max(...arr)
