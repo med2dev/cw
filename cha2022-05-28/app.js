79806b3ad2a80f68443c82c2d72539878bdfdc19
med2dev <daniel.bryce.hall@gmail.com>
2022-05-28 13:06:02 -0700

    May 28 challenge

---
+++ cha2022-05-28/app.js
@@ -0,0 +1,8 @@
+// P: an array of integers
+// R: the minimum value
+
+// E: 
+min([-2, 4, 2, -7, 3]) // -3
+
+// P: this is a builtin Math method, you just need to use spread notation
+const min = arr => Math.min(...arr)
