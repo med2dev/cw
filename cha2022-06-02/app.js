7e96afb114729289fc175b61d759184f643e82e7
med2dev <daniel.bryce.hall@gmail.com>
2022-06-02 13:47:30 -0700

    June 2 challenge

---
+++ cha2022-06-02/app.js
@@ -0,0 +1,13 @@
+// P: two string representations of integers
+// R: a string representation of the sum of those integers
+
+// E:
+
+
+function sumStr(a, b){
+    // coerce to number
+    const numA = Number(a)
+    const numB = Number(b)
+    // sum and coerce back to string
+    const result = String(numA + numB)
+}
