9191b62e9b471de6491c209799932970c2017c3b
med2dev <daniel.bryce.hall@gmail.com>
2022-06-06 00:23:21 -0700

    --date=2021-11-17

---
+++ cha2021-11-17/app.js
@@ -0,0 +1,14 @@
+// P: an integer, from 1 to 12, representing the month of the year
+
+// R: an integer, representing the quarter of the year the month is in
+
+// E:
+quarterOf(3) // 1
+quarterOf(12) // 4
+
+
+function quarterOf(m){
+    // P: no need to go through the months. This is just a simple math problem
+    // x/12 = y/4, so y = x/3
+    return Math.ceil(m/3)
+}
