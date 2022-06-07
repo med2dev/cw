fcd8182a630689512f896a1eaa456865c09c5fb6
med2dev <daniel.bryce.hall@gmail.com>
2022-06-07 01:42:02 -0700

    November 5 challenge

---
+++ cha2021-11-05/app.js
@@ -0,0 +1,7 @@
+// simply assign the variables and then sum them, inline
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
