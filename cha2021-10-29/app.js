045c2fa3464a121c0672cb0e18290e2f4df32edc
med2dev <daniel.bryce.hall@gmail.com>
2022-06-07 22:33:54 -0700

    October 29 challenge

---
+++ cha2021-10-29/app.js
@@ -0,0 +1,12 @@
+// P: an array
+// R: the array, with each positive element of the input negative, each 
+//      negative element of the input positive
+
+// E: 
+invert([1, 2, 3]) // [-1, -2, -3]
+invert([1, -2, 3]) // [-1, 2, -3]
+
+function invert(array){
+    // map
+    
+}
